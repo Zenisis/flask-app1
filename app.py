63b0bd7c187bb56915f2720eb75868ab6191f52e
zenisis <zenisis@example>
2025-09-01 11:38:22 +0530

    code changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Jenkins CI/CD with Flask!"
+    return "Hello, from kartik to Jenkins CI/CD with Flask!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
